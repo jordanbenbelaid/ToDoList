74b69f687137511e65b3cf25bd8b57034bb95739
jordanbenbelaid <jordanbenbelaid@hotmail.com>
2021-03-12 15:30:46 +0000

    urgency services class done

---
+++ ToDoList/src/main/java/com/example/toDoListTasks/service/UrgencyService.java
@@ -0,0 +1,74 @@
+package com.example.toDoListTasks.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.toDoListTasks.data.model.Urgency;
+import com.example.toDoListTasks.data.repository.UrgencyRepository;
+import com.example.toDoListTasks.dto.UrgencyDTO;
+import com.example.toDoListTasks.exceptions.TaskNotFoundException;
+import com.example.toDoListTasks.mappers.UrgencyMapper;
+
+@Service
+public class UrgencyService {
+
+	private UrgencyRepository urgencyRepository;
+	private UrgencyMapper urgencyMapper;
+	
+	@Autowired
+	public UrgencyService(UrgencyRepository urgencyRepository, UrgencyMapper urgencyMapper) {
+		this.urgencyRepository = urgencyRepository;
+		this.urgencyMapper = urgencyMapper;
+	}
+	
+	@Transactional
+	public List<UrgencyDTO> readAllPonds(){
+		List<Urgency> urgencyInDb = urgencyRepository.findAll();
+		List<UrgencyDTO> urgencyReturns = new ArrayList<UrgencyDTO>();
+		
+		urgencyInDb.forEach(urgency -> {urgencyReturns
+			.add(urgencyMapper.mapToUrgencyDTO(urgency));
+		});
+		
+		return urgencyReturns;
+	}
+	
+	public UrgencyDTO createUrgency(Urgency urgency) {
+		Urgency savedUrgency = urgencyRepository.save(urgency);
+		
+		return urgencyMapper.mapToUrgencyDTO(savedUrgency);
+	}
+	
+	public UrgencyDTO updateUrgency(Integer id, Urgency urgency) {
+		if(!urgencyRepository.existsById(id)) {
+			throw new TaskNotFoundException();
+		}
+		
+		Optional<Urgency> urgencyOpt = urgencyRepository.findById(id);
+		Urgency urgencyInDb = urgencyOpt.orElseThrow(() -> {
+			throw new TaskNotFoundException();		
+		});
+		
+		urgencyInDb.setName(urgency.getName());
+		
+		return urgencyMapper.mapToUrgencyDTO(urgencyRepository.save(urgencyInDb));
+	}
+	
+	public Boolean deleteUrgency(Integer id) {
+		if(urgencyRepository.existsById(id)) {
+			urgencyRepository.deleteById(id);
+		} else {
+			throw new TaskNotFoundException();
+		}
+		
+		boolean doesItExistStill = urgencyRepository.existsById(id);
+		
+		return !doesItExistStill;
+	}
+}
